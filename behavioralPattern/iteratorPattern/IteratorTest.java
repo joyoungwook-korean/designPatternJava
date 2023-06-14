3761ff52b9511126851bb12e2f00f2ed97d86a17
joyoungwook-korean <siosio24@naver.com>
2023-06-14 16:50:00 +0900

    add git ignore

--- behavioralPattern/iteratorPattern/IteratorTest.java
+++ behavioralPattern/iteratorPattern/IteratorTest.java
@@ -1,19 +1,21 @@
 package behavioralPattern.iteratorPattern;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import behavioralPattern.iteratorPattern.iterator.Iterator;
 import behavioralPattern.iteratorPattern.repository.NameRepository;
 
 public class IteratorTest {
+    Logger logger = LoggerFactory.getLogger(IteratorTest.class);
 
     @Test
     public void iteratorTest() {
         NameRepository nameRepository = new NameRepository();
-
         for (Iterator iterator = nameRepository.getIterator(); iterator.hasNext();) {
             String name = (String) iterator.next();
-            System.out.println(name);
+            logger.info(name);
         }
     }
 }
